39ba7b4265ab23273f524e4cc4a61df8b8a8a2cb
Edward Romano <oudeis.metis@gmail.com>
2016-09-03 16:32:33 -0400

    Fixed errors with dropbox upload

--- missed-moment.py
+++ missed-moment.py
@@ -7,12 +7,29 @@ from decouple import config
 from gpiozero import Button
 from gpiozero import LED
 
+FILE_CHUNK_SIZE = 10 * 1024 * 1024  # 10MB
 DROPBOX_API_KEY = config('DROPBOX_API_KEY', default=None)
 
 
 button = Button(17)
 led = LED(4)
 dbx = dropbox.Dropbox(DROPBOX_API_KEY)
+
+
+def upload_dropbox_file_chucks(file_path, file_size):
+    f = open(file_path, 'rb')
+    upload_session = dbx.files_upload_session_start(f.read(FILE_CHUNK_SIZE))
+    cursor = dropbox.files.UploadSessionCursor(
+        session_id=upload_session.session_id, offset=f.tell())
+    commit = dropbox.files.CommitInfo(path=file_path)
+    while f.tell() < file_size:
+        if ((file_size - f.tell()) <= FILE_CHUNK_SIZE):
+            dbx.files_upload_session_finish(
+                f.read(FILE_CHUNK_SIZE), cursor, commit)
+        else:
+            dbx.files_upload_session_append(
+                f.read(FILE_CHUNK_SIZE), cursor.session_id, cursor.offset)
+            cursor.offset = f.tell()
 
 
 def capture_video():
@@ -29,11 +46,18 @@ def capture_video():
     # Convert to .mp4 and upload
     clean_file = '/tmp/' + file_name + '.mp4'
     os.system('MP4Box -fps 30 -add {} {}'.format(raw_file, clean_file))
-    dbx.files_upload('foo', clean_file)
+
+    file_size = os.path.getsize(clean_file)
+    if file_size <= FILE_CHUNK_SIZE:
+        f = open(clean_file, 'rb')
+        dbx.files_upload(f, clean_file)
+    else:
+        upload_dropbox_file_chucks(clean_file, file_size)
 
     led.off()
 
 
+# Main Function
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
     stream = picamera.PiCameraCircularIO(camera, seconds=60)
