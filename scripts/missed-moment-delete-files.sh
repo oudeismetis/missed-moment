4080c51ba855fc708237976991b64013e5a81db8
cherryshoe <cherryshoe@cs-mbp.local>
2021-03-06 08:25:08 -0500

    fix missed-moment-delete-files unable to find config.py, cron working directory is home folder where the app was instaled

--- scripts/missed-moment-delete-files.sh
+++ scripts/missed-moment-delete-files.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# This script will be called via a cron, so the working directory will be the home folder of the $USER defined in install.sh
+current_dir=`pwd`
+missed_moment_dir=$current_dir/missed-moment/missed_moment
 
 # source config file
-. ../config.py
+. $missed_moment_dir/config.py
 
-echo MEDIA_DIR $MEDIA_DIR, DAYS_TO_KEEP_FILES $DAYS_TO_KEEP_FILES
+echo current_dir $current_dir, missed_moment_dir $missed_moment_dir, MEDIA_DIR $MEDIA_DIR, DAYS_TO_KEEP_FILES $DAYS_TO_KEEP_FILES
 
 # create crontab to look for missed moment media files that are
 # older than DAYS_TO_KEEP_FILES plus 1 day.  If DAYS_TO_KEEP_FILES is 6 (mtime +6 is 7 days ago) and deletes it.
